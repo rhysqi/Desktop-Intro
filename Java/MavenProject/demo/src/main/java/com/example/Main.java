425fbfef6f917132ba4fc471313a9b9cc6b13bd5
Risky Akbar <rhysqi.akbar@gmail.com>
2023-08-30 00:14:33 +0700

    Add example java desktop
    Adding java swing with gradle and maven

---
+++ Java/MavenProject/demo/src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame FBase = new JFrame();
+
+        FBase.setSize(800, 500);
+        FBase.setBounds(500, 500, 800, 500);
+        FBase.setLayout(null);
+        FBase.setVisible(true);
+    }
+}
